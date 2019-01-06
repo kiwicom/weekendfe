090891d68045dc770e64d50c351a5885b046a3f0
Petr Snobelt <petr.snobelt@kiwi.com>
2019-01-06 16:56:41 +0100

    Add eslint, prettier

--- pages/_document.js
+++ pages/_document.js
@@ -1,26 +1,26 @@
-import Document, { Head, Main, NextScript } from "next/document";
-import { ServerStyleSheet } from "styled-components";
+import Document, { Head, Main, NextScript } from "next/document"
+import { ServerStyleSheet } from "styled-components"
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet();
+    const sheet = new ServerStyleSheet()
 
-    const originalRenderPage = ctx.renderPage;
+    const originalRenderPage = ctx.renderPage
     ctx.renderPage = () =>
       originalRenderPage({
         enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
-      });
+      })
 
-    const initialProps = await Document.getInitialProps(ctx);
+    const initialProps = await Document.getInitialProps(ctx)
     return {
       ...initialProps,
       styles: [...initialProps.styles, ...sheet.getStyleElement()]
-    };
+    }
   }
 
   render() {
     return (
-      <html>
+      <html lang="en-GB">
         <Head>
           <link
             href="https://fonts.googleapis.com/css?family=Roboto:400,400i,500,500i,700"
@@ -32,6 +32,6 @@ export default class MyDocument extends Document {
           <NextScript />
         </body>
       </html>
-    );
+    )
   }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,9 @@
-import Alert from "@kiwicom/orbit-components/lib/Alert";
+import Alert from "@kiwicom/orbit-components/lib/Alert"
+import Illustration from "@kiwicom/orbit-components/lib/Illustration"
 
-export default () => <Alert>Hello, Welcome to JS WEEKEND!</Alert>;
+export default () => (
+  <>
+    <Alert type="success">Hello, Welcome to JS WEEKEND!</Alert>
+    <Illustration size="medium" name="Accommodation" dataTest="test" spaceAfter={null} />
+  </>
+)
