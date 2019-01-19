09fd178e5dba1975421e56b667496c278bdd0374
Petr Snobelt <petr.snobelt@kiwi.com>
2019-01-19 09:44:17 +0100

    remove unused fragments

--- pages/interests.js
+++ pages/interests.js
@@ -7,34 +7,31 @@ import CardSectionContent from "@kiwicom/orbit-components/lib/Card/CardSection/C
 import BrnoInterests from "../queries/interests.gql"
 
 const InterestsSample = () => (
-  <>
-    <Query
-      query={BrnoInterests}
-      context={{ uri: "https://weekend-api.now.sh/" }}
-    >
-      {({ loading, error, data }) => {
-        if (error) return <div>error</div>
-        if (loading) return <div>Loading</div>
-        return data.interests.map(
-          ({ img, score, name, category, address, url }) => (
-            <Card key={`${name}-${address}`}>
-              <CardSection>
-                <CardSectionHeader>
-                  <em>{category}</em> <a href={url}>{name}</a> [
-                  {score}]
-                </CardSectionHeader>
-                <CardSectionContent>
-                  <img src={img} alt={name} />
-                  <br />
-                  {address}
-                </CardSectionContent>
-              </CardSection>
-            </Card>
-          )
+  <Query
+    query={BrnoInterests}
+    context={{ uri: "https://weekend-api.now.sh/" }}
+  >
+    {({ loading, error, data }) => {
+      if (error) return <div>error</div>
+      if (loading) return <div>Loading</div>
+      return data.interests.map(
+        ({ img, score, name, category, address, url }) => (
+          <Card key={`${name}-${address}`}>
+            <CardSection>
+              <CardSectionHeader>
+                <em>{category}</em> <a href={url}>{name}</a> [{score}]
+              </CardSectionHeader>
+              <CardSectionContent>
+                <img src={img} alt={name} />
+                <br />
+                {address}
+              </CardSectionContent>
+            </CardSection>
+          </Card>
         )
-      }}
-    </Query>
-  </>
+      )
+    }}
+  </Query>
 )
 
 export default InterestsSample
