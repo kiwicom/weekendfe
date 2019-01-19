158daa39103fe2d641e7cf76dad2a1f884b92d53
Petr Snobelt <petr.snobelt@kiwi.com>
2019-01-19 09:43:22 +0100

    add url to interests

--- pages/interests.js
+++ pages/interests.js
@@ -8,24 +8,30 @@ import BrnoInterests from "../queries/interests.gql"
 
 const InterestsSample = () => (
   <>
-    <Query query={BrnoInterests} context={{ uri: "https://weekend-api.now.sh/" }}>
+    <Query
+      query={BrnoInterests}
+      context={{ uri: "https://weekend-api.now.sh/" }}
+    >
       {({ loading, error, data }) => {
         if (error) return <div>error</div>
         if (loading) return <div>Loading</div>
-        return data.interests.map(({ img, score, name, category, address }) => (
-          <Card key={`${name}-${address}`}>
-            <CardSection>
-              <CardSectionHeader>
-                <em>{category}</em> {name} [{score}]
-              </CardSectionHeader>
-              <CardSectionContent>
-                <img src={img} alt={name} />
-                <br />
-                {address}
-              </CardSectionContent>
-            </CardSection>
-          </Card>
-        ))
+        return data.interests.map(
+          ({ img, score, name, category, address, url }) => (
+            <Card key={`${name}-${address}`}>
+              <CardSection>
+                <CardSectionHeader>
+                  <em>{category}</em> <a href={url}>{name}</a> [
+                  {score}]
+                </CardSectionHeader>
+                <CardSectionContent>
+                  <img src={img} alt={name} />
+                  <br />
+                  {address}
+                </CardSectionContent>
+              </CardSection>
+            </Card>
+          )
+        )
       }}
     </Query>
   </>
