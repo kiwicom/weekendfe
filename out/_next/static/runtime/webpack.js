09d0167b04c2fdb982fb9932a4f1d124f41d6959
Petr Snobelt <petr.snobelt@kiwi.com>
2019-01-23 15:46:54 +0100

    Deploy Next.js to gh-pages

--- out/_next/static/runtime/webpack.js
+++ out/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4b8ca1cf3695a3470283";
+/******/ 	var hotCurrentHash = "be2a87546c1eb6ad1401";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
