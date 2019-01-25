81469310f715cfdc20ec98febd403ce15ad983f3
Luděk Vepřek <weprous@gmail.com>
2019-01-25 23:27:33 +0100

    ui start

--- pages/downshift.js
+++ pages/downshift.js
@@ -1,19 +1,13 @@
-import { useState, useRef } from "react"
+import * as React from "react"
 import Heading from "@kiwicom/orbit-components/lib/Heading"
-import Stack from "@kiwicom/orbit-components/lib/Stack"
-import Checkbox from "@kiwicom/orbit-components/lib/Checkbox"
 import styled from "styled-components"
-import { format, addDays } from "date-fns"
+import { format } from "date-fns"
 import Router from "next/router"
 
 import ContentContainer from "../components/ContentContainer"
-import PlacePicker from "../components/PlacePickerLocations"
-import DatePicker from "../components/DatePicker"
 import Interests from "../components/Interests"
-import useOnClickOutside from "../components/useOnClickOutside"
 import PlacesToVisit from "../components/PlacesToVisit"
-import Stepper from "../components/Stepper"
-import useUrl from "../components/useUrl"
+import TopPart, { defaultValues } from "../components/TopPart"
 
 const NomadForm = styled.div`
   max-width: 816px;
@@ -22,148 +16,6 @@ const NomadForm = styled.div`
 const StyledOrigin = styled.div`
   max-width: 756px;
 `
-
-const today = new Date()
-const day = 60 * 60 * 24 * 1000
-
-const defaultValues = {
-  adults: 2,
-  interest: "drinks",
-  flyFrom: { id: "brno_cz", name: "Brno" },
-  dateFrom: new Date(),
-  dateTo: new Date(today.getTime() + day * 7),
-  places: [[{ name: "Italy", id: "IT", code: "IT" }, [2, 5]]]
-}
-
-const TopPart = ({ flyFrom, flyTo, dateFrom, dateTo, adults }) => {
-  const [tripFrom, setFrom] = useUrl(
-    flyFrom || defaultValues.flyFrom,
-    "flyFrom",
-    item => (item ? item.id : undefined)
-  )
-  const [tripAdults, setAdults] = useUrl(adults, "adults")
-
-  const [showDestination, setDestinationVisibility] = useState(
-    Boolean(flyTo)
-  )
-  const [tripTo, setDestination] = useUrl(flyTo, "flyTo", item =>
-    item ? item.id : undefined
-  )
-
-  const [showReturnDate, setReturnDateVisibility] = useState(
-    Boolean(dateTo)
-  )
-  const [departureDate, setDepartureDate] = useUrl(
-    dateFrom ? new Date(dateFrom) : defaultValues.dateFrom,
-    "dateFrom",
-    date => format(date, "YYYY-MM-DD")
-  )
-  const [
-    departureDatePickerOpened,
-    setDepartureDatePickerVisibility
-  ] = useState(false)
-
-  const [returnDate, setReturnDate] = useUrl(
-    dateTo ? new Date(dateTo) : addDays(new Date(), 10),
-    "dateTo",
-    date => (date ? format(date, "YYYY-MM-DD") : undefined)
-  )
-  const [
-    returnDatePickerOpened,
-    setReturnDatePickerVisibility
-  ] = useState(false)
-
-  const selectDepartureDate = date => {
-    setDepartureDate(date.date)
-    setDepartureDatePickerVisibility(false)
-  }
-
-  const selectReturnDate = date => {
-    setReturnDate(date.date)
-    setReturnDatePickerVisibility(false)
-  }
-
-  const departureDatepickerRef = useRef()
-  useOnClickOutside(departureDatepickerRef, () =>
-    setDepartureDatePickerVisibility(false)
-  )
-
-  const returnDatepickerRef = useRef()
-  useOnClickOutside(returnDatepickerRef, () =>
-    setReturnDatePickerVisibility(false)
-  )
-
-  return (
-    <Stack spaceAfter="largest">
-      <Stack direction="row" justify="end">
-        <Stack basis="calc(25% - 16px)" shrink={false} grow={false}>
-          <Stepper
-            min={1}
-            max={20}
-            defaultValue={tripAdults}
-            onChange={setAdults}
-          />
-        </Stack>
-      </Stack>
-      <Stack direction="row">
-        <PlacePicker
-          label="From"
-          defaultValue={tripFrom}
-          onChange={setFrom}
-        />
-        <DatePicker
-          openRef={departureDatepickerRef}
-          label="Departure"
-          onFocus={() => setDepartureDatePickerVisibility(true)}
-          shown={departureDatePickerOpened}
-          currentDate={departureDate}
-          onDateSelected={selectDepartureDate}
-        />
-      </Stack>
-      {(showDestination || showReturnDate) && (
-        <Stack direction="row">
-          {showDestination && (
-            <PlacePicker
-              label="To"
-              defaultValue={tripTo}
-              onChange={setDestination}
-            />
-          )}
-          {showReturnDate && (
-            <DatePicker
-              openRef={returnDatepickerRef}
-              label="Arrival"
-              onFocus={() => setReturnDatePickerVisibility(true)}
-              shown={returnDatePickerOpened}
-              currentDate={returnDate}
-              onDateSelected={selectReturnDate}
-            />
-          )}
-        </Stack>
-      )}
-      <Stack direction="row">
-        <Checkbox
-          label="Return to origin"
-          checked={!showDestination}
-          onChange={e => {
-            setDestinationVisibility(!e.target.checked)
-            setDestination(null)
-          }}
-        />
-        <Checkbox
-          label="Set return date"
-          checked={showReturnDate}
-          onChange={e => {
-            setReturnDateVisibility(e.target.checked)
-            setReturnDate(
-              showReturnDate ? null : defaultValues.dateTo
-            )
-          }}
-        />
-      </Stack>
-    </Stack>
-  )
-}
 
 const placesToUrl = places =>
   places
