e96deeac2a2fb63d473958d8fb868678280e1acd
Luděk Vepřek <weprous@gmail.com>
2019-05-27 17:56:47 +0200

    ENV Map token

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 /* eslint-disable no-param-reassign */
 const webpack = require("webpack")
 const withBundleAnalyzer = require("@zeit/next-bundle-analyzer")
+const dotenv = require("dotenv").config()
 
 const debug = process.env.NODE_ENV !== "production"
-const repoName = ""
 
 const nextConfig = {
+  publicRuntimeConfig: {
+    mapToken: dotenv.parsed.MAP_TOKEN // Pass through env variables
+  },
   analyzeServer: ["server", "both"].includes(
     process.env.BUNDLE_ANALYZE
   ),
