bb870e55d536803a6eb69ace79f41e4fffc30c0c
Luděk Vepřek <weprous@gmail.com>
2019-05-27 16:59:04 +0200

    fix

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /* eslint-disable no-param-reassign */
-const webpack = require("webpack")
 const withBundleAnalyzer = require("@zeit/next-bundle-analyzer")
-
-const debug = process.env.NODE_ENV !== "production"
-const repoName = ""
 
 const nextConfig = {
   analyzeServer: ["server", "both"].includes(
