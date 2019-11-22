449f20f77c36267a15cdc84584fcbb83c83484c8
Jaroslav Kubicek <jaroslav.kubicek@gmail.com>
2019-11-22 22:54:11 +0100

    fix flow for Autocomplete

--- src/components/Autocomplete/Autocomplete.js
+++ src/components/Autocomplete/Autocomplete.js
@@ -3,7 +3,11 @@
 import * as React from "react"
 import { Stack, Tile } from "@kiwicom/orbit-components"
 
-const Autocomplete = ({ search }) => {
+type Props = {|
+  search: string,
+|}
+
+const Autocomplete = ({ search }: Props) => {
   return (
     <Stack>
       <Tile title={search} />
