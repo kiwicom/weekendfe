19c8fa8fb29a056d8672f3ff7079ed49c54ad015
Jaroslav Kubicek <jaroslav.kubicek@gmail.com>
2019-11-22 23:49:44 +0100

    Add generated flow types

--- src/components/Autocomplete/Autocomplete.js
+++ src/components/Autocomplete/Autocomplete.js
@@ -1,10 +1,14 @@
 // @flow
 
 import * as React from "react"
-import { Stack, Tile } from "@kiwicom/orbit-components"
+import { Alert, Stack, Tile } from "@kiwicom/orbit-components"
 import { QueryRenderer, graphql } from "@adeira/relay"
 
 import { weekendapiEnvironment } from "../../services/enviroment"
+import type {
+  AutocompleteQueryResponse,
+  AutocompleteQueryVariables
+} from "./__generated__/AutocompleteQuery.graphql"
 
 type Props = {|
   search: string
@@ -26,16 +30,30 @@ const Autocomplete = ({ search }: Props) => {
       <QueryRenderer
         environment={weekendapiEnvironment}
         query={query}
-        variables={{ query: search }}
-        onResponse={data =>
-          data.locations.map(location => (
-            <Tile
-              key={location.id}
-              title={location.name}
-              description={`Type: ${location.type}`}
-            />
-          ))
-        }
+        variables={({ query: search }: AutocompleteQueryVariables)}
+        onResponse={(data: AutocompleteQueryResponse) => {
+          if (!data.locations || data.locations.length === 0) {
+            return <Alert type="info">Sorry, no locations!</Alert>
+          }
+
+          return data.locations.map(location => {
+            if (!location) {
+              return null
+            }
+
+            return (
+              <Tile
+                key={location.id}
+                title={location.name}
+                description={
+                  location.type != null
+                    ? `Type: ${location.type}`
+                    : "Unkown type"
+                }
+              />
+            )
+          })
+        }}
       />
     </Stack>
   )
