f841167ec79b05e27ae66d5cc80bebca73420068
Jaroslav Kubicek <jaroslav.kubicek@gmail.com>
2019-11-23 17:58:23 +0100

    Add solution to unit test for Time component

--- src/components/TestedComponents/__tests__/Time.js
+++ src/components/TestedComponents/__tests__/Time.js
@@ -1,6 +1,22 @@
 // @flow
 /* eslint-env node, jest */
 
+import * as React from "react"
+import { mount } from "enzyme"
+import { advanceTo } from "jest-date-mock"
+
+import Time from "../Time"
+
 describe("Time", () => {
-  it.todo("Shows that Alan was late")
+  beforeEach(() => {
+    advanceTo(new Date(2020, 11, 31))
+  })
+
+  it("Shows that Alan was late", () => {
+    const component = mount(
+      <Time name="Alan" arrival={new Date(2048, 11, 31)} />
+    )
+
+    expect(component.find("Text").prop("type")).toBe("warning")
+  })
 })
